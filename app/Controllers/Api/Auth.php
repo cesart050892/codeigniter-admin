6f4801501d2dfe6a52247fbe16d3facfe3bb1c76
cesart050892 <cesart050892@gmail.com>
2021-08-20 23:17:56 -0600

    create login method

--- app/Controllers/Api/Auth.php
+++ app/Controllers/Api/Auth.php
@@ -53,4 +53,48 @@ class Auth extends ResourceController
 			return $this->failServerError($e->getMessage());
 		}
 	}
+
+
+	public function login()
+	{
+		//
+		try {
+			$rules_income = [ // Rules validations
+				'username' => 'required',
+				'password' => 'required|min_length[4]', //validate_user[username,password]
+			];
+			if ($this->validate($rules_income)) { // Execute validation
+				$data = [$this->request->getVar('username'), $this->request->getVar('password')];
+				$results = $this->model->where('username', $data[0])->first(); // Verify exist username
+				if ($results == null) return $this->fail('This user no exist!');
+				if (password_verify($data[1], $results->password)) { // Verify password
+					$session_data = [ // Session data
+						'base_url'  => base_url(),
+						'user_img'  => $results->img,
+						'user_name' => $results->name,
+						'user_fullname' => $results->display_name,
+						'user_id' => $results->id,
+						'isLoggedIn' => true
+					];
+					session()->set($session_data);
+					return $this->respond([
+						'status' => 200,
+						'message' => 'logged in',
+						'data' => [
+							'username' => $results->username,
+							'name' =>  $results->display_name,
+							'created_at' => $results->created_at->humanize(),
+						]
+					], 200);
+				} else {
+					return $this->failValidationErrors('Invalid password');
+				}
+			} else {
+				return $this->fail($this->validator->getErrors());
+			}
+		} catch (\Throwable $e) {
+			//throw $e;
+			return $this->failServerError($e->getMessage());
+		}
+	}
 }
