4b91313a2187e544d50ccc0a695d9793bfe6c487
cesart050892 <cesart050892@gmail.com>
2021-08-20 18:42:26 -0600

    style the login page to our needs

--- app/Views/app/auth/login.php
+++ app/Views/app/auth/login.php
@@ -5,11 +5,80 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('body') ?>
-
 <body class="hold-transition login-page">
-    <?= $this->endSection() ?>
+<?= $this->endSection() ?>
 
-    <?= $this->section('content') ?>
+<?= $this->section('style') ?>
+<style>
+  body {
+    /* background: #3a7bd5 !important;
+    background-image: -webkit-radial-gradient(bottom, circle cover, #00d2ff 0%, #3a7bd5 80%) !important; */
+    background: rgb(40, 80, 150);
+    background: radial-gradient(circle, rgba(40, 80, 150, 1) 0%, rgba(0, 50, 120, 1) 90%);
+    /* background: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 30, 50, 1)) !important; */
+
+    display: flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+  }
+
+  .bg-img__ {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background: url('../utils/img/login-page.jpg');
+    background-size: cover;
+    overflow: hidden;
+    z-index: -1;
+  }
+
+  .loader-container {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    background: #000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .loader {
+    width: 50px;
+    height: 50px;
+    border: 5px solid;
+    color: #3a7bd5;
+    border-radius: 50%;
+    border-top-color: transparent;
+    animation: loader 1s linear infinite;
+  }
+
+  @keyframes loader {
+    0% {
+      color: #181818;
+    }
+
+    50% {
+      color: #2d88c3;
+    }
+
+    75% {
+      color: #f7b017;
+    }
+
+    100% {
+      color: #ec3b1f;
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
     <div class="login-box">
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
@@ -19,12 +88,12 @@
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
 
-                <form action="../../index3.html" method="post">
+                <form autocomplete="off">
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="user" class="form-control" placeholder="User">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
+                                <span class="fas fa-user"></span>
                             </div>
                         </div>
                     </div>
@@ -53,16 +122,6 @@
                     </div>
                 </form>
 
-                <div class="social-auth-links text-center mt-2 mb-3">
-                    <a href="#" class="btn btn-block btn-primary">
-                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                    </a>
-                    <a href="#" class="btn btn-block btn-danger">
-                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                    </a>
-                </div>
-                <!-- /.social-auth-links -->
-
                 <p class="mb-1">
                     <a href="forgot-password.html">I forgot my password</a>
                 </p>
@@ -75,4 +134,4 @@
         <!-- /.card -->
     </div>
     <!-- /.login-box -->
-    <?= $this->endSection() ?>
+<?= $this->endSection() ?>
